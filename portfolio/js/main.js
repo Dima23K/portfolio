e81af5e86de420f6097363da49b7e76146e63510
Dima23K <dmokotedi47@gmail.com>
2017-11-23 12:26:51 +0200

    Add files via upload

---
+++ portfolio/js/main.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function() {
+			$('.scrollTo').click( function() { 
+				var page = $(this).attr('href'); 
+				var speed = 1200; 
+				$('html, body').animate( { scrollTop: $(page).offset().top }, speed );
+				return false;
+			});
+		});
